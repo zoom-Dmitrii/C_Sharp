f65ce9a37001e6994a59ef7a61bb18e7ec186d7c
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-04 02:37:29 +0300

    Ex7 - Задача 7. Вывод последней цифры трехзначного числа

---
+++ Example/Ex7/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+int n1 = 0;
+while (n1 < 100) {
+    Console.WriteLine("введите трехзначное число");
+    n1 = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine(n1 % 10);
