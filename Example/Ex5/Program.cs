270657c69cbf7f32d7ae9b74931ff00537461505
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-04 02:28:57 +0300

    Ex5 - Задача 5. Вывод чисел от -N до N

---
+++ Example/Ex5/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("введите число");
+int n1 = Convert.ToInt32(Console.ReadLine());
+int nstart = 0;
+if (n1 > 0) 
+ nstart = (-1) * n1;
+ else {
+    nstart = n1;
+    n1 = (-1) * n1;
+ }
+Console.WriteLine($"Целые числа в диапозоне от {nstart} до " + n1);
+for(int i=nstart; i <= n1; i++) {
+    Console.Write(" " + i + " ");
+}
