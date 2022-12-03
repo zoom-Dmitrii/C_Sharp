ea627adbfd504ff99d5fe5a6863708aa3e0f57cd
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-04 01:23:57 +0300

    Ex2 - Задача 2. Поиск максимального

--- Example/Ex2/Program.cs
+++ Example/Ex2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("введите первое число");
+double n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+double n2 = Convert.ToInt32(Console.ReadLine());
+if (n1 > n2){
+    Console.WriteLine($"Первое число больше второго. max {n1} ");
+}
+else{
+    Console.WriteLine($"Второе число больше первого. max {n2} ");
+}
