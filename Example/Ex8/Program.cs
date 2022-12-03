e4b6b24425f1865610014b8bb8789e497d7a720a
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-04 01:48:42 +0300

    Ex8 - Задача 8. Четные числа в диапозоне от 1 до N

---
+++ Example/Ex8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("введите число");
+double n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Четные числа в диапозоне от 1 до {n1} ");
+for(int i=1; i <= n1; i++) {
+ if (i % 2 == 0){
+    Console.Write(" " + i + " ");    
+ }
+}
