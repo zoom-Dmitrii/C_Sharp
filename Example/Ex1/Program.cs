884bb185c15c5648c9d21209276ed4160632e077
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-04 02:04:07 +0300

    Ex1 - Задача 1. Является ли первое число квадратом второго

--- Example/Ex1/Program.cs
+++ Example/Ex1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("введите первое число");
+double n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+double n2 = Convert.ToInt32(Console.ReadLine());
+if (n2 * n2 == n1){
+    Console.WriteLine($"Число {n2} является квадрвтом числа " + n1);    
+}
+else {
+Console.WriteLine($"Число {n2} не является квадрвтом числа " + n1);
+}
