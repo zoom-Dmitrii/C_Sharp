d36519f58cde870858beac65af053ccd908679bc
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-04 01:40:53 +0300

    Ex6 - Задача 6. Определить четное или нечетное число

---
+++ Example/Ex6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("введите число");
+double n1 = Convert.ToInt32(Console.ReadLine());
+if (n1 % 2 ==0){
+    Console.WriteLine($"Число четное ");
+}
+else{
+    Console.WriteLine($"Число нечетное ");
+}
