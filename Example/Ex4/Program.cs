001315eaad5cbcd26edf962f050f24339f51c8f9
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-04 01:35:49 +0300

    Ex4 - Задача 4. Поиск максимального из трех чисел

---
+++ Example/Ex4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("введите первое число");
+double n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+double n2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите третье число");
+double n3 = Convert.ToInt32(Console.ReadLine());
+if ((n1 > n2) && (n1 > n3)){
+    Console.WriteLine($"Первое число максимальное. max {n1} ");
+}
+else{
+    if ((n2 > n1) && (n2 > n3)) {
+        Console.WriteLine($"Второе число максимальное. max {n2} ");
+    }
+    else {
+        Console.WriteLine($"Третье число максимальное. max {n3} ");
+    }
+}
