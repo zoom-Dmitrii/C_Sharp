f7ace6583030ff3219e238fb82985c91b65f2099
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-04 14:56:13 +0300

    Ex21 - Задача 21. Длина отрзка

---
+++ Example/Ex21/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Координата первой точки:");
+Console.WriteLine("Введите координату X1:");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату Y1:");
+double y1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Координата второй точки:");
+Console.WriteLine("Введите координату X2:");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату Y2:");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(Math.Sqrt(Math.Pow((x1-x2),2) + Math.Pow((y1-y2),2)));
