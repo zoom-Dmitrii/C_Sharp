cfc1c8c8db2e3cf86b65815ea17380cbc7195d69
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-04 14:20:32 +0300

    Ex17 - Задача 17.Вывод четверти по x и y

---
+++ Example/Ex17/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.WriteLine("Введите координату Х:");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату Y:");
+double y = Convert.ToDouble(Console.ReadLine());
+while (x == 0)
+{
+    Console.Write("Вы ошиблись!\nВведите координату X: ");
+    x = Convert.ToDouble(Console.ReadLine());
+}
+while (y == 0)
+{
+    Console.Write("Вы ошиблись!\nВведите координату Y: ");
+    y = Convert.ToDouble(Console.ReadLine());
+}
+if (x > 0 && y > 0)
+    Console.WriteLine("I");
+else if (x < 0 && y > 0)
+    Console.WriteLine("II");
+else if (x < 0 && y < 0)
+    Console.WriteLine("III");
+else
+    Console.WriteLine("IV");
