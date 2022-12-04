0b09a97bcc1f27b5dc23d1f1fc6649862ffc89d9
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-04 13:44:47 +0300

    ExS-2-1. Семинар 2. Вывод большей цифры числа

---
+++ Example/ExS-2-1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+int number = new Random().Next(10,100);
+Console.WriteLine($"Сгенерированное число{number}");
+int n1 = number / 10;
+int n2 = number % 10;
+
+if (n1 > n2){
+    Console.WriteLine($"Наибольшая цифра числа - {n1}");
+}
+else{
+    Console.WriteLine($"Наибольшая цифра числа - {n2}");
+}
