56e70106597a0cb137fce2fde86b788ff85585f7
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-04 14:18:32 +0300

    Ex18 - Задача 18. Вывод x и у по четверти

---
+++ Example/Ex18/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите номер четверти");
+int n = Convert.ToInt32(Console.ReadLine());;
+
+while (n < 1 || n > 4)
+{
+    Console.Write("Вы ошиблись!\nВведите ччисла 1 2 3 4 ");
+    n = Convert.ToInt32(Console.ReadLine());;
+}
+if (n == 1)
+    Console.WriteLine("x > 0 && y > 0");
+else if (n == 2)
+    Console.WriteLine("x < 0 && y > 0");
+else if (n == 3)
+    Console.WriteLine("x < 0 && y < 0");
+else
+    Console.WriteLine("x > 0 && y < 0");
