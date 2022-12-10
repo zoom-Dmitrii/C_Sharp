887ac2d17affb865ccb473ce7b533f676cf74095
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-10 14:07:37 +0300

    Ex15 Введенный день недели выходно?

---
+++ Example/Ex15/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+int n1 = 0;
+while (n1 < 1 || n1 > 7) {
+    Console.WriteLine("введите день недели числом от 1 до 7");
+    n1 = Convert.ToInt32(Console.ReadLine());
+}
+if (n1 == 7 || n1 == 6) {
+    Console.WriteLine("Вы ввели выходной день");
+}
