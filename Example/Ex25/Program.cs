b4635d32507feda9e9a05212e2c7c508343ef793
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-10 21:17:40 +0300

    Ex25 Возведение числ A в степень B

---
+++ Example/Ex25/Program.cs
@@ -0,0 +1,17 @@
+﻿int f(int a, int b)
+{
+    int c = 1;
+    while (b != 0) {
+    c *= a;
+    b--;
+    }
+    return c;
+}
+Console.Clear();
+Console.WriteLine("Возведение числ A в степень B");
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b < 0) b *= -1;
+Console.WriteLine($"Числ A в степени B: {f(a, b)}");
