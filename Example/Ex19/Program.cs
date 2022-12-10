32c7a6b1f94a1c62faa139bbb7a9cddeece99ade
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-10 18:25:06 +0300

    Ex19 Введенное число полином?

---
+++ Example/Ex19/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+string number = Console.ReadLine(), revers = string.Empty;
+//int n = Convert.ToInt32(number);
+for (int i = number.Length-1; i >= 0; i--) {
+revers += number[i];
+}
+if (revers.Equals(number)) 
+Console.Write("Число полиндром");
+else Console.Write("Число НЕ полиндром");
