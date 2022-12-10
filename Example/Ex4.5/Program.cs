648d0b30901cd3de688f3108557eb9e9c1e2e26b
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-10 17:51:47 +0300

    Ex4.5 Функция работа со значением по ссылке

---
+++ Example/Ex4.5/Program.cs
@@ -0,0 +1,19 @@
+﻿void f(ref int a)
+{
+    a += 5;
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+f(ref a);
+Console.WriteLine(a);
+/*
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+f(ref a);
+Console.WriteLine(a);
+
+*/
