e77c9ba535e5bede6db70fdf80bbd4ea8b9beaa8
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-10 17:46:37 +0300

    Ex30 Заполнить массив 0 или 1 произвольно

---
+++ Example/Ex30/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+int n = 8;
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(0,2); // [0, 2]
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
