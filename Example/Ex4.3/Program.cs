e59841166615e26ce8be2a2b670576f53816028e
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-10 17:52:59 +0300

    Ex4.3 Функция значения - копия данных

---
+++ Example/Ex4.3/Program.cs
@@ -0,0 +1,16 @@
+﻿int f(int a, int b=10)
+{
+    if (a > b)
+        return a;
+    return b;
+}
+
+// return возвращает значение либо в консоль, либо в переменную
+// return завершает работу функции
+// Сколько аргументов мы передаем, столько и принимаем. НО!!!
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(a));
