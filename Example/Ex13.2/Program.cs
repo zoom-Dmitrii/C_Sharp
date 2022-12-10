ba4a1248036b04dc38bbfa906d5061ddd4d8b8b8
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-10 13:53:14 +0300

    Ex13.2 Вывод третьей цифры любого числа

---
+++ Example/Ex13.2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+int n1 = 0;
+    Console.WriteLine("введите трехзначное число");
+    n1 = Convert.ToInt32(Console.ReadLine());
+    if (n1 < 100) {
+        Console.WriteLine("третьей цифры нет");
+    } else {
+          while (n1 >= 1000) n1 /= 10;
+         Console.WriteLine(n1 % 10);
+//Console.WriteLine((n1 / 10) %10);
+    }
+
