6dd8f303c086a0960522e53877c2e79b24caee81
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-10 22:56:02 +0300

    Ex29 Воод размера массива и числа массива

---
+++ Example/Ex29/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int [] num = new int[Convert.ToInt32(Console.ReadLine())];
+Console.WriteLine("Введите элементы массива:");
+for (int i = 0; i < num.Length; i++){
+num[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"[{string.Join(", ", num)}]");
