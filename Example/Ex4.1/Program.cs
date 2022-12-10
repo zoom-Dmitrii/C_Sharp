7e106b1f38bb52f84d32247862b7cd685536154b
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-10 20:09:28 +0300

    Ex4.1 Добавил информирование по программе

--- Example/Ex4.1/Program.cs
+++ Example/Ex4.1/Program.cs
@@ -1,7 +1,16 @@
-﻿int n = Convert.ToInt32(Console.ReadLine());
+﻿Console.WriteLine("Введите количество кустов, от 3 до 1000");
+int n = 0;
+while (n <3 || n > 1000)
+n = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[n];
-string[] input = Console.ReadLine().Split(" ");
-for (int i = 0; i < input.Length; i++)
+string[] input = new string[0];
+while (input.Length != array.Length){
+Console.WriteLine("Введите количество ягод на каждом кусте, от 1 до 1000");
+//Console.WriteLine("Ели количество ягод-кустов превысит количество введенных кустов - они не учтутся, от 3 до 1000");
+input = Console.ReadLine().Split(" ");
+}
+
+for (int i = 0; i < n; i++)
     array[i] = int.Parse(input[i].ToString());
 
 int maxSumma = 0;
@@ -17,4 +26,4 @@ if (array[0] + array[1] + array[array.Length - 1] > maxSumma)
 if (array[array.Length - 1] + array[array.Length - 2] + array[0] > maxSumma)
     maxSumma = array[array.Length - 1] + array[array.Length - 2] + array[0];
 
-Console.WriteLine(maxSumma);
+Console.WriteLine($"Максимальное количество ягод на трех соседних кустах: {maxSumma}");
