be4080d19760729cf1618c96b6b40540274d288b
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-10 22:07:51 +0300

    Ex25 Сумма цифр в числе

---
+++ Example/Ex27/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+int sum = 0;
+for (int i = 0; i < number.Length; i++) {
+sum += Convert.ToInt32(number[i].ToString());
+}
+Console.WriteLine($"Сумма цифр в числе: {sum}");
