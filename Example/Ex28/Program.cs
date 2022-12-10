afe6f8952283a8b9fae8239d9ab61e5120c6e371
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-10 17:48:44 +0300

    Ex28 произведение от 1 до N

---
+++ Example/Ex28/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine()), res = 1;
+for (int i = 2; i <= n; i++ ) {
+ res *= i;
+}
+Console.WriteLine(res);
