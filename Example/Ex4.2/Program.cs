d742ff1141c086e903ab82e2326e7e867d35954d
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-10 17:53:50 +0300

    Ex4.2 Массив - ввод и вывод строкой

---
+++ Example/Ex4.2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Array
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 11); // [1, 10]
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
