cc5fc6acff740058149023ee84b6632515fd4ed2
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-10 14:30:28 +0300

    dop2 Дополнительная задача к 2-му семинару

---
+++ Example/dop2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+int n = int.Parse(Console.ReadLine());
+int maxFirst = n, maxSecond = -1;
+while (n != 0)
+{
+    n = int.Parse(Console.ReadLine());
+    if (maxFirst < n)
+    {
+        maxSecond = maxFirst;
+        maxFirst = n;
+    }
+    else if (maxSecond < n)
+        maxSecond = n;
+}
+Console.WriteLine(maxSecond);
