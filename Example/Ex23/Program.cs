63924d56dbac19b9409173131aa53c7f7116855c
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-10 18:41:26 +0300

    Ex23 таблица кубов чисел от 1 до N

---
+++ Example/Ex23/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("введите число");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Кубы чисел в диапозоне от 1 до {n1} ");
+for(int i=1; i <= n1; i++) {
+    Console.WriteLine($"Число {i}, куб " + Math.Pow(i,3));    
+}
