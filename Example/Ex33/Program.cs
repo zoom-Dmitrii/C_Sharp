1b80f2418c2cc9e21a51949012c709eb9d9b5300
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-14 21:37:52 +0300

    Ex33 Массив, угадай число в массиве

---
+++ Example/Ex33/Program.cs
@@ -0,0 +1,23 @@
+﻿void InputArray (int [] array) {
+for (int i = 0; i < array.Length; i++)
+ array[i] = new Random().Next(0, 11); // [0, 10]
+}
+
+void ReleaseArray (int [] array, int number) {
+for (int i = 0; i < array.Length; i++)
+ if (array[i] == number) { 
+ Console.WriteLine("Вы угадали, есть такое число в массиве!");
+ return;
+}
+    Console.WriteLine("Вы НЕ угадали, такого числа в массиве нет"); 
+}
+
+
+Console.Clear();
+Console.Write("Укажите размер массива: ");
+int [] array = new int[Convert.ToInt32(Console.ReadLine())];
+InputArray(array);
+Console.Write("Укажите число от 0 до 10: ");
+int number = Convert.ToInt32(Console.ReadLine());
+ReleaseArray(array, number);
+Console.WriteLine($"[{string.Join(", ", array)}]");
