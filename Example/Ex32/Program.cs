164e638341d4897211e8426b24064f4ba783cee6
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-14 21:23:56 +0300

    Ex32 Массив, замена элементов на противоположные

---
+++ Example/Ex32/Program.cs
@@ -0,0 +1,19 @@
+﻿void InputArray (int [] array, int rasmer) {
+for (int i = 0; i < array.Length; i++)
+ array[i] = new Random().Next(rasmer * -1, rasmer + 1); // [-N, N]
+Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+
+void ReleaseArray (int [] array) {
+for (int i = 0; i < array.Length; i++)
+ array[i] *= -1;
+Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+
+
+Console.Clear();
+Console.Write("Случайное заполнение массива от -N до N. Укажите размерность N: ");
+int rasmer = Convert.ToInt32(Console.ReadLine());
+int [] array = new int[rasmer*2+1];
+InputArray(array, rasmer);
+ReleaseArray(array);
