8aed3c79be5412d6eb5521fac9f47c3fe4f0841b
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-14 21:53:44 +0300

    Ex35 Массив, число элементов в диапозоне от 10 до 99

---
+++ Example/Ex35/Program.cs
@@ -0,0 +1,22 @@
+﻿void InputArray (int [] array) {
+for (int i = 0; i < array.Length; i++)
+ array[i] = new Random().Next(0, 501); // [0, 500]
+}
+
+int ReleaseArray (int [] array) {
+    int count = 0;
+for (int i = 0; i < array.Length; i++)
+ if (array[i] >= 10 && array[i] <=99) 
+ ++count;
+ return count; 
+}
+
+
+Console.Clear();
+Console.WriteLine("Размер массива из 123 элементов");
+Console.WriteLine();
+int [] array = new int[122];
+InputArray(array);
+Console.Write($"Число элементов от 10 до 99 внутри массива: {ReleaseArray(array)}");
+Console.WriteLine();
+Console.WriteLine($"[{string.Join(", ", array)}]");
