859b4ace06f852445c1753d8c050b0c863a5df87
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-16 19:04:48 +0300

    Ex38 Сумма минимального и максимального элемента массива

---
+++ Example/Ex38/Program.cs
@@ -0,0 +1,20 @@
+﻿void InputArray (double [] array) {
+for (int i = 0; i < array.Length; i++)
+array[i] = Math.Round(new Random().NextDouble() + new Random().Next(1,100), 2); // [1, 99]
+Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+double ReleaseArray (double [] array) {
+double minNum = array[0], maxNum = array[0];
+for (int i = 1; i < array.Length; i++) {
+    if (array[i] < minNum) minNum = array[i];
+    else  if (array[i] > maxNum) maxNum = array[i];
+}
+return minNum + maxNum;
+}
+
+Console.Clear();
+Console.Write("Укажите размер массива: ");
+double [] array = new double[Convert.ToInt32(Console.ReadLine())];
+InputArray(array);
+Console.WriteLine();
+Console.WriteLine($"Сумма минимального и максимального элемента массива: {ReleaseArray(array)} ");
