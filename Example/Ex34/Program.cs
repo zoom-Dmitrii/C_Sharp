47103eb01773380b74abf942c53b271dbe0c8aaf
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-16 18:30:09 +0300

    Ex34 Количество четных чисел в массиве

---
+++ Example/Ex34/Program.cs
@@ -0,0 +1,19 @@
+﻿void InputArray (int [] array) {
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(100, 1000); // [100, 999]
+Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+int ReleaseArray (int [] array) {
+    int count = 0; 
+for (int i = 0; i < array.Length; i++) {
+    if (array[i] % 2 == 0) count ++; 
+}
+return count;
+}
+
+Console.Clear();
+Console.Write("Укажите размер массива: ");
+int [] array = new int[Convert.ToInt32(Console.ReadLine())];
+InputArray(array);
+Console.WriteLine();
+Console.WriteLine($"Количество четных чисел в массиве: {ReleaseArray(array)} ");
