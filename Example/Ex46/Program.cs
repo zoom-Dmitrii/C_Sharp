78e7fd0d6655a546c13cde203e7b15982d2cc277
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-18 14:47:49 +0300

    Ex46 Двумерный массив автозаполнение

---
+++ Example/Ex46/Program.cs
@@ -0,0 +1,18 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11); // [-10, 10]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
