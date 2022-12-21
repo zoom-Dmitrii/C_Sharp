dbd5acb80609cf6ae1a69f2e9baebec0969b8d21
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-21 20:21:05 +0300

    Ex40 Длины трех сторон являются сторонами треугольника?

---
+++ Example/Ex40/Program.cs
@@ -0,0 +1,13 @@
+﻿String TestSide(int[] sizeSide)
+{
+    if (sizeSide[0] < sizeSide[1] + sizeSide[2])
+        if (sizeSide[1] < sizeSide[0] + sizeSide[2])
+            if (sizeSide[2] < sizeSide[0] + sizeSide[1])
+                return "являются сторонами треугольника";
+    return "не являются сторонами треугольника";
+}
+
+Console.Clear();
+Console.Write("Введите длины трех сторон треугольника: ");
+int[] sizeSide = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+Console.WriteLine($"Введенные длины {TestSide(sizeSide)}");
