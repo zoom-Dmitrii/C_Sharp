ab580ce38192dafaf0c07b88650bec09ee06b3ef
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-21 19:48:38 +0300

    Ex39 Перевернутый массив

---
+++ Example/Ex39/Program.cs
@@ -0,0 +1,19 @@
+﻿void InputArray (int [] array) {
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(1,11); // [1, 10]
+}
+
+void ReleaseArray (int [] array) {
+for (int i = 0; i < array.Length / 2; i++) {
+    int temp = array[i];
+    array[i] = array[array.Length - 1 - i];
+    array[array.Length - 1 - i] = temp;
+}
+}Console.Clear();
+Console.Write("Укажите размер массива: ");
+int [] array = new int[Convert.ToInt32(Console.ReadLine())];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
+Console.WriteLine();
+Console.WriteLine($"[{string.Join(", ", array)}]");
