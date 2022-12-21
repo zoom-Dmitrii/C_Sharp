6764f282adc962dcc75826fff264867d726b6096
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-21 21:07:30 +0300

    Ex45 Поэлементное копирование массива

---
+++ Example/Ex45/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+int [] a = {1,2,3,4,5};
+int [] b = new int [a.Length];
+for (int i = 0; i < a.Length; i++)
+ b[i] = a[i];
+ b[0] = -5;
+ Console.WriteLine(string.Join(" ",a));
+ Console.WriteLine(string.Join(" ",b));
