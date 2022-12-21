cd78a2de93eedf367f0f5176911745462a188509
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-21 18:24:32 +0300

    Ex41 Сколько чисел больше 0

---
+++ Example/Ex41/Program.cs
@@ -0,0 +1,13 @@
+﻿int InputArray (int [] array) {
+    int countPositive = 0;
+for (int i = 0; i < array.Length; i++)
+ if (array[i] > 0)  ++countPositive;
+return countPositive;
+}
+
+Console.Clear();
+Console.WriteLine("Сколько чисел больше 0 \n Введите целые числа через пробел, завершение Enter: ");
+int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+Console.WriteLine($"[{string.Join(", ", array)}]");
+InputArray(array);
+Console.WriteLine(InputArray(array));
