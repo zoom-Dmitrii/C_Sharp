2a66c8fd099f0905f35f3d172aeb826461fbc624
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-21 20:42:28 +0300

    Ex44 Число Фибоначчи через цикл

---
+++ Example/Ex44/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), x1 = 1, x2 = 0, x = 0;
+for (int i = 0; i < n; i++)
+{
+x2 = x1;
+x1 = x;
+Console.Write($"{x} ");
+x = x1 + x2;
+} 
