a8c097650a757acc4a645a01f093d1fa6d8b858e
zoom Дмитрий <z-oo-m@yandex.ru>
2022-12-21 20:23:07 +0300

    Ex42 Преобразование десятмчного в двоичное

---
+++ Example/Ex42/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Clear();
+Console.Write("Введите десятичное число: ");
+String binary = string.Empty;
+int num = Convert.ToInt32(Console.ReadLine());
+while (num != 0)
+{
+   binary = binary.Insert(0,Convert.ToString(num % 2));
+   num = num / 2; 
+} 
+Console.Write($"Двоичное представление: {binary}");
