1145c96e2e42b4c394d0c09dab874288e379b847
zoom Дмитрий <z-oo-m@yandex.ru>
2023-01-13 20:45:20 +0300

    Ex63 Вывод натуральных чисел до заданного числа

---
+++ Example/Ex63/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Задайте число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вывод натуральных чисел до заданного числа");
+Console.WriteLine(PrintNumbers(n));
+
+string PrintNumbers(int end)
+{
+    if (end == 1)
+    {
+       // Console.WriteLine(1);
+        return "1";
+    }
+    string s = PrintNumbers(end - 1) + ' ' + end.ToString();
+    // Console.WriteLine(s);
+    return s;
+}
