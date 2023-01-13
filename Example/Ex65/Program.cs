4154e4c9f0a99434258b6952d361cd4ee0b8bc33
zoom Дмитрий <z-oo-m@yandex.ru>
2023-01-13 22:06:05 +0300

    Ex65 Вывод натуральных чисел от начального до конечного числел

---
+++ Example/Ex65/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Задайте начальное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте конечное число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вывод натуральных чисел от начального до конечного числел");
+Console.WriteLine(PrintNumbers(n, m));
+
+string PrintNumbers(int start, int end)
+{
+    if (end == start)
+    {
+        return start.ToString();
+    }
+    string s = PrintNumbers(start, end - 1) + ' ' + end.ToString();
+    return s;
+}
