3f0796ee91b8bb94a522ed7371703f909122100f
zoom Дмитрий <z-oo-m@yandex.ru>
2023-01-13 22:06:58 +0300

    Ex67 Вывод суммы цифр числа

---
+++ Example/Ex67/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Задайте число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вывод суммы цифр числа");
+Console.WriteLine(PrintNumbers(n));
+
+int PrintNumbers(int sum)
+{
+    if (sum == 0)
+    {
+        return 0;
+    }
+    int s = PrintNumbers(sum / 10) + sum % 10;
+    return s;
+}
