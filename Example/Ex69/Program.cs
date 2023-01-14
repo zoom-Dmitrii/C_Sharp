416fce2f6d611dc1bd2eb9a26467abcd1ce56b84
zoom Дмитрий <z-oo-m@yandex.ru>
2023-01-14 19:58:44 +0300

    Ex69 Вывод числа X в степени Y

---
+++ Example/Ex69/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Задайте число X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте число Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вывод числа X в степени Y");
+Console.WriteLine($"{Pow(x, y)}");
+
+int Pow(int num, int runk)
+{
+    if (runk == 0)
+    {
+        return 1;
+    }
+    return (num * Pow(num, runk - 1));
+}
