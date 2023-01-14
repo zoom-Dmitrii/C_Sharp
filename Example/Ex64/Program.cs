ee606966e22e58c7293574b0ff71bcedf9535202
zoom Дмитрий <z-oo-m@yandex.ru>
2023-01-14 20:12:38 +0300

    Ex64 Вывод натуральных чисел от N до M. В обратном порядке

---
+++ Example/Ex64/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Задайте начальное число M: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте конечное число N: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вывод натуральных чисел от N до M");
+Console.WriteLine(PrintNumbers(n, m));
+
+string PrintNumbers(int start, int end)
+{
+    if (end == start)
+    {
+        return start.ToString();
+    }
+    return PrintNumbers(start + 1, end) + ' ' + start.ToString();
+}
