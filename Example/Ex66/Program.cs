bcabcca693f78ec7dac332b4b591580103d3bd3c
zoom Дмитрий <z-oo-m@yandex.ru>
2023-01-14 21:32:16 +0300

    Ex66 Вывод суммы чисел от M до N

---
+++ Example/Ex66/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Задайте начальное число M: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте конечное число N: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вывод суммы чисел от M до N");
+Console.WriteLine($"{Summ(n, m)}");
+
+int Summ(int start, int end)
+{
+    if (end == start)
+    {
+        return start;
+    }
+    return (end + Summ(start, end-1));
+}
