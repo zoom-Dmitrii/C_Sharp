b2927a12efa35231486fcb60d7db188539548d15
zoom Дмитрий <z-oo-m@yandex.ru>
2023-01-14 22:24:27 +0300

    Ex68 Функция Аккермана A(m,n)

---
+++ Example/Ex68/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.Write("Задайте первое число m: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте второе число n: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Функция Аккермана A(m,n): ");
+Console.WriteLine($"{Ack(n, m)}");
+
+int Ack(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if ((m > 0) && (n == 0))
+    {
+        return Ack(m - 1, 1);
+    }
+    else if ((m > 0) && (n > 0))
+    {
+        return Ack(m - 1, Ack(m, n - 1));
+    }
+    else
+        return n + 1;
+}
